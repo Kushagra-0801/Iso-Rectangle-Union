11bda11a2af4a1a9cc117d48735fa372073a2bc1
shreyas0430 <f20180430@hyderabad.bits-pilani.ac.in>
2021-03-17 14:06:28 +0530

    Test cases are now getting written to points.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 #include "lib/common.hpp"
 #include "lib/generator.hpp"
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+
+	ofstream MyFile("points.txt");
 
     SetGenerator gen;
     const auto rects = gen.rects(5);
     for (auto &&r : rects) {
-        std::cout << r.upper_left.x << " " << r.upper_left.y << " : "
-                  << r.lower_right.x << " " << r.lower_right.y << std::endl;
+        MyFile << r.upper_left.x << " " << r.upper_left.y << " : "
+                  << r.lower_right.x << " " << r.lower_right.y << endl;
     }
+    MyFile.close();
     return EXIT_SUCCESS;
 }
